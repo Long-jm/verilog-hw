628e09035928085b1401f376c97b3219fd181dbe
Jon Mark Long <jmlong4@ncsu.edu>
2021-04-14 14:27:05 -0400

    Hw 7 commit

---
+++ homework/homework_007/adler32_acc.v
@@ -0,0 +1,30 @@
+module adler32_acc (
+  input clk, rst_n,
+  input [7:0] data,
+  output [31:0] checksum
+);
+
+  reg [15:0] Aold, Bold;
+  wire [15:0] Anew, Bnew, Mod;
+  
+  assign Mod = 16'd65521;
+
+  assign Anew = ((Aold + data) > Mod) ? ((Aold + data) - Mod) : (Aold + data);
+  assign Bnew = ((Anew + Bold) > Mod) ? ((Anew + Bold) - Mod) : (Anew + Bold);
+  assign checksum = {Bold, Aold};
+
+  always @( posedge clk ) begin
+    if( !rst_n )
+      Aold <= 16'h0001;
+    else
+      Aold <= Anew;
+  end
+  
+  always @( posedge clk ) begin
+    if( !rst_n )
+      Bold <= 16'h0000;
+    else
+      Bold <= Bnew;
+  end
+   
+endmodule
